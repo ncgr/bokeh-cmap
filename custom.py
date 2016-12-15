986625c14f87682eeb89c14342bde598aeba42da
Alex Rice <agr@ncgr.org>
2016-12-14 17:14:42 -0700

    Remove older demo/practice files.

--- custom.py
+++
@@ -1,8 +0,0 @@
-from bokeh.core.properties import String, Instance
-from bokeh.models import LayoutDOM, Slider
-from bokeh.util.compiler import FromFile
-
-class Custom(LayoutDOM):
-    __implementation__ = FromFile('custom.js')
-    text = String(default="Custom text")
-    slider = Instance(Slider)
--- models/map_spine.js
+++ models/map_spine.js
@@ -8,4 +8,3 @@ class MapSpineView extends RectView {
 export class MapSpine extends Rect {}
 MapSpine.prototype.default_view = MapSpineView;
 MapSpine.prototype.type = 'MapSpine'; // name of the Python class
-// MapSpineGlyph.coords([['x', 'y']])
